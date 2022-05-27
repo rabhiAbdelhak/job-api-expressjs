e20cab0f8db37d4895596078450efb80fcb7506e
abdelhak <abdelhakrabhi0@gmail.com>
2022-05-27 21:05:32 +0100

    not found update

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,3 +1,3 @@
-const notFound = (req, res) => res.status(404).send('Route does not exist')
+const notFound = (req, res) => res.status(404).json({msg : 'Route does not exist', route : req.url})
 
 module.exports = notFound
